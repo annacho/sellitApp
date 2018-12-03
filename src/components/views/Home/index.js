c9687ab62b1b8f53097c21c2bd6e5b3f8566ddce
annacho <annacho13@gmail.com>
2018-12-03 14:30:09 -0500

    Add horizontal scrolling

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 import { navigatorDrawer, navigatorDeepLink } from '../../utils/misc';
+import HorizontalScroll from './horizontal_scroll_icons';
 
 class Home extends Component {
   constructor(props){
     super(props);
 
+    this.state = {
+      categories:['All','Sports','Music','Clothing','Electronics'],
+      categorySelected:"All"
+    }
+
     this.props.navigator.setOnNavigatorEvent((event)=>{
+      navigatorDeepLink(event,this)
       navigatorDrawer(event,this)
     })
 
+  updateCategoryHandler = (value) => {
+    this.setState({
+      categorySelected:value
+    })
+  }
+
   render() {
     return (
-      <div>
-      <Text>Home</Text>
-      </div>
+      <ScrollView>
+        <View style={styles.container}>
+          <HorizontalScroll
+            categories={this.state.categories}
+            categorySelected={this.state.categorySelected}
+            updateCategoryHandler={this.updateCategoryHandler}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
